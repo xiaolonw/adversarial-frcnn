2662c653cc537d90e628d6f99b3d6ac786922281
Xiaolong Wang <xiaolonw@cs.cmu.edu>
2017-04-21 19:58:20 -0400

    update protobuf

--- lib/fast_rcnn/train.py
+++ lib/fast_rcnn/train.py
@@ -16,6 +16,7 @@ import os
 
 from caffe.proto import caffe_pb2
 import google.protobuf as pb2
+import google.protobuf.text_format
 
 class SolverWrapper(object):
     """A simple wrapper around Caffe's solver.
